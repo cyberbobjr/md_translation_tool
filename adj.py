de449351dc2f0b1a03c31f4a99a879eefd785f75
benjaminmarchand <cyberbobjr@yahoo.com>
2022-11-23 09:34:38 +0100

    Improve tools for adj generation

--- adj.py
+++ adj.py
@@ -7,28 +7,37 @@ This script is used for generating ADJ for French translation :
 - all data must be in /common folder
 """
 
-regex = r"\s{1,}(?P<country>.*)_[fascism|democratic|neutrality|communism|nationalist].*"
+regex = r"\s{1,}(?P<country>.*)_[fascism|democratic|neutrality|communism|nationalist]_?.*?\:\s\"(?P<trad>.*)\""
 exclude = "autonomy"
-countries = []
+countries = {}
 prefix = 'FRLOC'
-adjectives = ['INHAB', 'ADJ_MS', 'ADJ_FS', 'ADJ_MP', 'ADJ_FP']
+adjectives = ['ADJ_MS', 'ADJ_FS', 'ADJ_MP', 'ADJ_FP','INHAB']
 template = """
     text = {
             trigger = { has_cosmetic_tag = %%COUNTRY%% }
             localization_key = %%ADJ%%
         }
 """
+keyfilename = "0_FR_loc_l_french_tomerge.yml"
+
 content = open("common/MD_countries_cosmetic_l_french.yml")
 for line in content.readlines():
     matches = re.finditer(regex, line, re.MULTILINE)
     for matchNum, match in enumerate(matches, start=1):
         country = match.group("country")
+        trad = match.group("trad")
         if exclude.lower() not in country.lower() and country not in countries and "#" not in country:
-            countries.append(country)
+            countries[country] = trad
 
-print(countries)
 for adj in adjectives:
     output = open(f"common/{adj}.txt", "w+")
-    for country in countries:
-        build = f"{prefix}_{country}_{adj}"
-        print(template.replace("%%COUNTRY%%", country).replace("%%ADJ%%", build), file=output)
+    for key in countries:
+        build = f"{prefix}_{key}_{adj}"
+        print(template.replace("%%COUNTRY%%", key).replace("%%ADJ%%", build), file=output)
+
+keyoutput = open(f"{keyfilename}", "w+")
+for key in countries:
+    for adj in adjectives:
+        build = f"{prefix}_{key}_{adj}"
+        print(f" {build} : \"{countries[key]}\"", file=keyoutput)
+    print(f"\r", file=keyoutput)
